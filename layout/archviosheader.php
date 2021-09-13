f80482e8bb24f66183ce0fd68521d2029d7e3396
brayanrojas21 <80425297+brayanrojas21@users.noreply.github.com>
2021-09-13 11:14:27 -0500

    Actualización

--- layout/archviosheader.php
+++ layout/archviosheader.php
@@ -2,7 +2,7 @@
 <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
 <link rel="stylesheet" href="css/all.min.css">
 <link rel="stylesheet" href="css/animate.min.css">
-<link rel="stylesheet" href="css/style.css">
+<link rel="stylesheet" href="css/style.css?v=1">
 <link rel="stylesheet" href="css/owl.carousel.min.css">
 <link rel="stylesheet" href="css/owl.theme.default.min.css">
 <link rel="stylesheet" href="css/animate.min.css">
--- layout/menu.php
+++ layout/menu.php
@@ -47,29 +47,23 @@
 </script>
 <!-- Fin de codigo preloader -->
 <div class="fixed-top total-menu">
-    <div class="container-fluid bg-light bg-transparent d-none d-lg-block" id="menu_contacto">
-        <div class="row justify-content-lg-center">
-            <div class="col-8 offset-2">
-                <div class="information">
-                    <small> <a href=""></a><i
-                            class="fas fa-map-marker-alt m-2"></i><?php echo $datos_contacto['direccion']['direccion'] ?></small>
-                    <small><a href="mailto:<?php echo $datos_contacto['correo']['correo'] ?>"><i
-                                class="far fa-envelope m-2"></i><?php echo $datos_contacto['correo']['correo'] ?></a></small>
-                    <small class="ml-5"><a href="tel:<?php echo $datos_contacto['celular']['link'] ?>"><i
-                                class="fas fa-mobile-alt m-2"></i><?php echo $datos_contacto['celular']['imprimir'] ?></a>
-                    </small>
-                    <small class=""><a href="tel:<?php echo $datos_contacto['telefono_fijo']['link'] ?>"><i
-                                class="fas fa-phone-alt m-2 colo_insta"></i><?php echo $datos_contacto['telefono_fijo']['imprimir'] ?></a></small>
-                    <small class=""><a target="_blank" href="<?php echo $datos_contacto['whatsapp']['link'] ?>"><i
-                                class="fab fa-whatsapp m-2 color_twi"></i><?php echo $datos_contacto['whatsapp']['imprimir'] ?></a></small>
-                </div>
+    <div class="bg-light bg-transparent d-none d-lg-block" id="menu_contacto">
+        <div class="container d-flex justify-content-lg-around information ">
+            <div>
+                <small><a href=""></a><i class="fas fa-map-marker-alt m-2"></i><?php echo $datos_contacto['direccion']['direccion'] ?></small>
+                <small><a href="mailto:<?php echo $datos_contacto['correo']['correo'] ?>"><i class="far fa-envelope m-2"></i><?php echo $datos_contacto['correo']['correo'] ?></a></small>
+            </div>
+            <div>
+                <small><a href="tel:<?php echo $datos_contacto['celular']['link'] ?>"><i class="fas fa-mobile-alt m-2"></i><?php echo $datos_contacto['celular']['imprimir'] ?></a></small>
+                <small><a href="tel:<?php echo $datos_contacto['telefono_fijo']['link'] ?>"><i class="fas fa-phone-alt m-2 colo_insta"></i><?php echo $datos_contacto['telefono_fijo']['imprimir'] ?></a></small>
+                <small><a target="_blank" href="<?php echo $datos_contacto['whatsapp']['link'] ?>"><i class="fab fa-whatsapp m-2 color_twi"></i><?php echo $datos_contacto['whatsapp']['imprimir'] ?></a></small>
             </div>
         </div>
     </div>
     <nav class="navbar navbar-expand-lg navbar-light bg-light bg-transparent" id='menu'>
         <div class="container">
             <a class="navbar-brand" href="./">
-                <img src="images/Logo_Blanco.png" width="auto" height="80" alt="" id="imagen_menu">
+                <img src="images/Logo_Blanco.png" width="auto" height="70" alt="" id="imagen_menu">
             </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown"
                 aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
