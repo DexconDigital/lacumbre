f164836cbedb37f19790689274df075cbc5e57a2
brayanrojas21 <80425297+brayanrojas21@users.noreply.github.com>
2021-09-24 12:02:08 -0500

    Add files via upload

--- controllers/indexDB.php
+++ controllers/indexDB.php
@@ -5,4 +5,3 @@ require_once("conexion.php");
 $con = Conect();
 $qry = "SELECT * FROM slide_inicio where id_inmobiliaria5 = '10'";
 $sql = mysqli_query($con,$qry);
-$resultado =  mysqli_fetch_all($sql);
